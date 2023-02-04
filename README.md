5c17a649e1159385ba5d79c34def6bbc2ba4ddfb
Khashayar Shomali <khashi792017@gmail.com>
2023-02-04 22:25:55 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Learn How to Use Local Storage in Simple Example
+
+### See Local Storage in Your Browser
+
+Just go to the developer tools by pressing F12, then go to the Application tab. In the Storage section expand Local Storage. After that, you'll see all your browser's local storage there.
---
+++ app.js
@@ -0,0 +1,54 @@
+// Without Local Storage
+const inputAreaWithoutLocalStorage = document.querySelector(
+  ".input-area-without-local-storage"
+);
+
+const inputTextValueWithoutLocalStorage = document.querySelector(
+  ".input-text-value-without-local-storage"
+);
+
+inputAreaWithoutLocalStorage.addEventListener(
+  "keyup",
+  displayWithoutLocalStorage
+);
+
+function displayWithoutLocalStorage() {
+  inputTextValueWithoutLocalStorage.innerHTML =
+    inputAreaWithoutLocalStorage.value;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// With Local Storage
+const inputAreaWithLocalStorage = document.querySelector(
+  ".input-area-with-local-storage"
+);
+
+const inputTextValueWithLocalStorage = document.querySelector(
+  ".input-text-value-with-local-storage"
+);
+
+inputTextValueWithLocalStorage.innerHTML =
+  localStorage.getItem("valueOfTextInput");
+inputAreaWithLocalStorage.addEventListener("keyup", displayWithLocalStorage);
+
+function displayWithLocalStorage() {
+  localStorage.setItem("valueOfTextInput", inputAreaWithLocalStorage.value);
+  /* This is For Test */ console.log(localStorage.getItem("valueOfTextInput"));
+  inputTextValueWithLocalStorage.innerHTML =
+    localStorage.getItem("valueOfTextInput");
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// Clear Local Storage
+const clearLocalStorage = document.querySelector(".remove-local-storage");
+
+clearLocalStorage.addEventListener("click", removeLocalStorage);
+
+function removeLocalStorage() {
+  window.localStorage.clear("valueOfTextInput");
+  inputTextValueWithLocalStorage.innerHTML = "";
+}
